7ca85ea0e6889667a73993a66a61a2913cb59030
hidden-dimensions <hidden-dimensions@users.noreply.github.com>
2018-10-12 12:14:23 +0800

    conf filename is an option #21

--- src/py-bandit/check.sh
+++ src/py-bandit/check.sh
@@ -3,6 +3,7 @@
 function main {
     # parse command line arguments
     local VERBOSE=yes
+    local CONF_FILE=
     local TARGET_DIR=
 
     for i in "$@"
@@ -20,7 +21,11 @@ function main {
             VERBOSE="${i#*=}"
             shift
             ;;
-        -d=*|--directory=*)
+        --conf-file=*)
+            CONF_FILE="${i#*=}"
+            shift
+            ;;
+        --target-dir=*)
             TARGET_DIR="${i#*=}"
             shift
             ;;
@@ -31,26 +36,29 @@ function main {
     done
     # validate arguments
     if [ ! -d "$TARGET_DIR" ]; then
-        echo "Missing argument -d"
+        echo "Missing argument --target-dir"
+        show_help
         exit 1
     fi
 
     echo "[ ] py-bandit"
-    check "$TARGET_DIR" "$VERBOSE"
+    check "$VERBOSE" "$CONF_FILE" "$TARGET_DIR"
 }
 
 function show_help {
-    echo "USAGE: check.sh [-v|--verbose] [-h|--help] -d=/path/to/your/code"
+    echo "USAGE: check.sh [-v|--verbose] [-h|--help] [--conf-file=.devsecops-ci] --target-dir=/path/to/your/code"
 }
 
 function check {
-    local TARGET_DIR=$1
-    local VERBOSE=$2
-    local VERBOSE_FLAG=
+    local VERBOSE=$1
+    local CONF_FILE=$2
+    local TARGET_DIR=$3
     local INI_FLAG=
 
-    if [ -f "${TARGET_DIR}/devsecops-ci.conf" ]; then
-        INI_FLAG="--ini ${TARGET_DIR}/devsecops-ci.conf"
+    if [ "$CONF_FILE" != "" ]; then
+      if [ -f "${TARGET_DIR}/${CONF_FILE}" ]; then
+        INI_FLAG="--ini ${TARGET_DIR}/${CONF_FILE}"
+      fi
     fi
     bandit ${INI_FLAG} -r "${TARGET_DIR}"
 }
--- src/py-safety/check.sh
+++ src/py-safety/check.sh
@@ -3,6 +3,7 @@
 function main {
     # parse command line arguments
     local VERBOSE=yes
+    local CONF_FILE=
     local TARGET_DIR=
 
     for i in "$@"
@@ -20,7 +21,11 @@ function main {
             VERBOSE="${i#*=}"
             shift
             ;;
-        -d=*|--directory=*)
+        --conf-file=*)
+            CONF_FILE="${i#*=}"
+            shift
+            ;;
+        --target-dir=*)
             TARGET_DIR="${i#*=}"
             shift
             ;;
@@ -31,32 +36,36 @@ function main {
     done
     # validate arguments
     if [ ! -d "$TARGET_DIR" ]; then
-        echo "Missing argument -d"
+        echo "Missing argument --target-dir"
+	show_help
         exit 1
     fi
 
     echo "[ ] py-safety"
-    check "$TARGET_DIR" "$VERBOSE"
+    check "$VERBOSE" "$CONF_FILE" "$TARGET_DIR"
 }
 
 function show_help {
-    echo "USAGE: check.sh [-v|--verbose] [-h|--help] -d=/path/to/your/code"
+    echo "USAGE: check.sh [-v|--verbose] [-h|--help] [--conf-file=.devsecops-ci] --target-dir=/path/to/your/code"
 }
 
 function check {
-    local TARGET_DIR=$1
-    local VERBOSE=$2
+    local VERBOSE=$1
+    local CONF_FILE=$2
+    local TARGET_DIR=$3
     local EXTRA_ARGS=
     local EXCLUDE=
     local FILES=
     local EXCLUDED_FILES=
 
-    if [ -f "${TARGET_DIR}/devsecops-ci.conf" ]; then
-        EXTRA_ARGS=`sed -e 's/[[:space:]]*\:[[:space:]]*/:/g' "${TARGET_DIR}/devsecops-ci.conf" \
+    echo "[I] conf: ${TARGET_DIR}/${CONF_FILE}"
+
+    if [ -f "${TARGET_DIR}/${CONF_FILE}" ]; then
+        EXTRA_ARGS=`sed -e 's/[[:space:]]*\:[[:space:]]*/:/g' "${TARGET_DIR}/${CONF_FILE}" \
                     | sed -n '/^\[py-safety\]/,/^\[.*\]/p' \
                     | grep "^[[:space:]]*arguments[[:space:]]*:" \
                     | sed 's/.*\:[[:space:]]*//'`
-        EXCLUDE=`sed -e 's/[[:space:]]*\:[[:space:]]*/:/g' "${TARGET_DIR}/devsecops-ci.conf" \
+        EXCLUDE=`sed -e 's/[[:space:]]*\:[[:space:]]*/:/g' "${TARGET_DIR}/${CONF_FILE}" \
                  | sed -n '/^\[py-safety\]/,/^\[.*\]/p' \
                  | grep "^[[:space:]]*exclude[[:space:]]*:" \
                  | sed 's/.*\:[[:space:]]*//' \
